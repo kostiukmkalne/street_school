76575c9f74e5761acf4ffa3b5033df90b7920aae
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-10-23 15:12:37 -0500

    create first page2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
 import { Navigation } from './components/Navigation/Navigation';
+import { Banner } from './components/Banner/Banner';
 
 export const App = () => {
   <div>
     <Header />
     <Navigation />
+    <Banner />
   </div>
 };
